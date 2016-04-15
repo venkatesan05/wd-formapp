041b98130cff425372d69243fa22a546d5054e3c
Venkatesan <venkatesan.log@gmail.com>
2016-04-15 11:03:30 +0530

    Source file

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>WD Form</title>
+<link rel="stylesheet" href="wdf.css" type="text/css" />
+
+</head>
+<body> 
+
+<?php require('wd_dom_builder.php'); ?>
+
+<h2 class="page-title"> <center>Sample Signup Form</center> </h2>
+
+<div class="form-container"> 
+
+<?php
+           $form = new WD_elements();
+
+$wdForm = $form->openForm('index.php', 'signup_form', 'post', array('class'=>'signupForm') ).
+
+            $form->inputType('text', 'firstname', '', array('id'=>'firstname', 'label' => 'Firstname', 'req' => 'req' ) ).
+
+            $form->inputType('text', 'lastname', '', array('id'=>'lastname', 'label' => 'Lastname' ) ).
+
+            $form->inputType('email', 'email', '', array('id'=>'email', 'label' => 'Email Address' ) ).
+
+            $form->inputType('password', 'password', '', array('id'=>'password', 'label' => 'Password' ) ).
+
+            $form->inputType('number', 'age', '', array('id'=>'age', 'label' => 'Age' ) ).
+
+            $form->inputType('file', 'prf_image', '', array('id'=>'prf_image', 'label' => 'Profile Image' ) ).
+
+            $form->groupTitle('Gender:') .
+            $form->inputType('radio', 'gender', 'male', array('id'=>'gender1', 'label' => 'Male', 'req' => 'req' ) ).
+            $form->inputType('radio', 'gender', 'female', array('id'=>'gender2', 'label' => 'Female', 'req' => 'req' ) ).
+
+            $form->groupTitle('Interest:') .
+            $form->inputType('checkbox', 'interest[]', 'Sports', array('id'=>'interest1', 'label' => 'Sports', 'req' => 'req' ) ).
+            $form->inputType('checkbox', 'interest[]', 'Learning', array('id'=>'interest2', 'label' => 'Learning' , 'req' => 'req' ) ).
+
+            $form->setTextArea('comments', '', array('id'=>'comments', 'placeholder'=>'Enter your comments..', 'req' => 'req') ) .              
+   
+            $form->selectBox('country', array(
+                                                   ''=>'Select country',
+                                                'ind'=>'India',
+                                                'fra'=>'France',
+                                                'lka'=>'Sri Lanka',
+                                                'fra'=>'France',
+                                                'usa'=>'United States' 
+                                            ),
+                                        array('id'=>'country', 'class'=>'country locations', 'req' => 'req')
+                            ).
+
+            $form->inputType('submit', 'signup', 'Sign Up') .
+
+            $form->closeForm();
+    
+    echo $wdForm;
+?>
+</div>
+
+</body>
+</html>
---
+++ wd_dom_builder.php
@@ -0,0 +1,152 @@
+<?php
+
+class WD_elements {
+    
+    private $tag; 
+    
+    function openForm($formAction = '#', $name=NULL, $formMethod = 'post', $attributes = array() ) {
+
+        $tag = "<form action=\"$formAction\" method=\"$formMethod\" name=\"$name\""; 
+
+        $tag .= $attributes? $this->setAttributes( $attributes ) . '>': '>';
+
+        return $tag;
+    } 
+
+    function inputType($type=NULL, $name=NULL, $value=NULL, $attributes = array() ) { 
+
+        $label = ''; $req = isset($attributes['req']) ? $attributes['req'] : ''; 
+
+        if(!in_array($type, array("button", "submit"))){
+
+            $labelText = isset($attributes['label']) ? $attributes['label'] : ucfirst($name);
+
+                $label = $this->setLabel($labelText);
+               
+                unset($attributes['label']);
+                unset($attributes['req']);
+        }
+
+        $nameAttr = substr($name, 0, -2);
+
+        $tag = $label.'<input type="'.$type.'" name="'.$name.'" value="'.$value.'"';
+        if ($attributes) {
+            $tag .= $this->setAttributes( $attributes );
+        }
+        $tag .= '>';
+
+        if($req == 'req' && isset($_REQUEST[$nameAttr]) && isset($_REQUEST[$nameAttr]) && $type == 'checkbox' ){
+
+             $tag .= $this->setErrorLabel($name);
+
+        } else       
+
+        if( $req == 'req' && isset($_REQUEST[$name]) && $_REQUEST[$name] == ''){  
+
+            $tag .= $this->setErrorLabel($name);
+
+        }       
+
+
+        $tag = $this->tagGroup($tag);
+        return $tag;
+    }
+    
+    function setTextarea($name=NULL, $value = '', $attributes = array() ) {
+
+        $rowsCols = ' rows = "4" cols = "30" ';
+
+        $req = isset($attributes['req']) ? $attributes['req'] : '';
+
+        $labelText = isset($attributes['label']) ? $attributes['label'] : ucfirst($name);
+        $label = $this->setLabel($labelText);
+        unset($attributes['label']);
+
+        $tag = $label."<textarea name=\"$name\"  $rowsCols ";
+
+        if ($attributes) {
+            $tag .= $this->setAttributes( $attributes );
+        }
+        $tag .= ">$value</textarea>";
+
+        if( $req == 'req' && isset($_REQUEST[$name]) && ($_REQUEST[$name] == '' || $_REQUEST[$name] == NULL) ){ 
+             $tag .= $this->setErrorLabel($name); 
+        }
+
+        $tag = $this->tagGroup($tag);
+        return $tag;
+    } 
+
+    function selectBox($name=NULL, $options_list=NULL, $attributes = array() ) {
+
+        $labelText = isset($attributes['label']) ? $attributes['label'] : ucfirst($name);
+
+        $req = isset($attributes['req']) ? $attributes['req'] : '';
+
+        $select = $this->setLabel($labelText);
+        unset($attributes['label']);        
+
+        $select .= "<select name=\"$name\" ";
+
+        if ($attributes) {
+            $select .= $this->setAttributes( $attributes );
+        }
+
+        $select .= " >";
+
+        foreach ( $options_list as $val => $option ) {
+
+            $selected = in_array($val, $attributes) ? " selected=\"selected\" ": " ";
+
+             $select .= "\n<option value=\"$val\" $selected >".$option."</option>"; 
+        }
+        $select .= "\n</select>";
+
+        if( $req == 'req' && isset($_REQUEST[$name]) && ($_REQUEST[$name] == '' || $_REQUEST[$name] == NULL) ){ 
+             $select .= $this->setErrorLabel($name); 
+        }
+
+        $select = $this->tagGroup($select);
+        return ($select);
+    }
+    
+    function tagGroup($tags) { 
+        $form_elem = "<div class=\"form-group\" >".$tags."</div >"; 
+        return $form_elem;
+    }  
+
+    function setLabel($label_text) { 
+        $label = '<label class="wdf-label">'.$label_text.'</label>'; 
+        return $label;
+    } 
+
+    function setErrorLabel($name) { 
+        $tag = "<small class=\"errorText\"> ".$name." field is required</small>";
+        return $tag;
+    }        
+
+
+    function groupTitle($title){
+
+        $group_title = '<h3 class="grp-title">'.$title.'</h3>'; 
+        return $group_title;
+    }
+
+    private function setAttributes( $attributes ) {
+
+        $attr = ''; 
+
+        foreach( $attributes as $key=>$val ) { 
+                $attr .=  $key.'="'.$val.'" '; 
+        }
+        return $attr;
+    }
+
+    function closeForm() { 
+
+        return "</form>";
+    }      
+    
+}
+
+?>
